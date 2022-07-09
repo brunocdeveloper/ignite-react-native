aa6e2375ccc48e34cd407221c3147df2e8bc2ab4
brunomorais <bruno.morais@gmail.com.br>
2022-07-09 12:46:36 -0300

    Adiciona tipagem para o button

---
+++ projetos/myskills/src/components/Button.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+}
+
+export function Button(props: ButtonProps) {
+  const {onPress, title} = props;
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      activeOpacity={0.6}
+      onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
---
+++ projetos/myskills/src/pages/Home.tsx
@@ -0,0 +1,88 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+  FlatList,
+  ScrollView,
+} from 'react-native';
+import {Button} from '../components/Button';
+import {SkillCard} from '../components/SkillCard';
+
+interface SkillData {
+  id: string;
+  name: string;
+}
+
+export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+
+  function handleNewAddSkill() {
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill,
+    };
+    setMySkills(prevState => [...prevState, data]);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Bruno!</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+        onChangeText={setNewSkill}
+      />
+
+      <Button onPress={handleNewAddSkill} title="Add" />
+
+      <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
+
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => <SkillCard skill={item.name} />}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  buttonSkill: {
+    alignItems: 'center',
+    backgroundColor: '#1F1E25',
+    padding: 20,
+    borderRadius: 50,
+    marginVertical: 10,
+  },
+  textSkill: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
