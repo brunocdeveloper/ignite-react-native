78e625cc1cbf1be105c85d4aedf499b131612849
brunomorais <bruno.morais@gmail.com.br>
2022-07-16 16:49:03 -0300

    Adiciona tipagem para o componente e estilizaçao dinamica com base nas props, adiciona tipagem para os componentes estilizados do card

--- projetos/gofinances/src/screens/Dashboard/index.tsx
+++ projetos/gofinances/src/screens/Dashboard/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
 import HighlightCard from "../../components/HighlightCard";
-
 import {
   Container,
   Header,
@@ -37,9 +35,24 @@ export default function Dashboard() {
       </Header>
 
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
       </HighlightCards>
     </Container>
   );
