184ff79e4ed869f143378c6f5ea2a7383a4e7f9a
brunomorais <bruno.morais@gmail.com.br>
2022-07-16 12:45:51 -0300

    Implementa o header da página

--- projetos/gofinances/src/screens/Dashboard/index.tsx
+++ projetos/gofinances/src/screens/Dashboard/index.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Container, Header } from "./styles";
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+} from "./styles";
 
 export default function Dashboard() {
   return (
     <Container>
       <Header>
-        <Text>Rodrigo</Text>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/72170223?v=4",
+              }}
+            />
+
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Bruno</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
       </Header>
     </Container>
   );
