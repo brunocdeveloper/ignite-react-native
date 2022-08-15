b5588b5ec6d1182e1229817e202d88043cdcd93f
brunomorais <bruno.morais@gmail.com.br>
2022-08-15 19:36:04 -0300

    Cria componente que renderiza a lista de categorias

--- projetos/gofinances/App.js
+++ projetos/gofinances/App.js
@@ -10,7 +10,7 @@ import {
   Poppins_500Medium,
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
-import { Register } from "./src/screens/Register";
+import { CategorySelect } from "./src/screens/CategorySelect";
 
 export default function App() {
   SplashScreen.preventAutoHideAsync();
@@ -28,7 +28,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <CategorySelect />
     </ThemeProvider>
   );
 }
