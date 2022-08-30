7110b50dee8f3a9d5984c5b0a8f2503b4d6101ed
brunomorais <bruno.morais@gmail.com.br>
2022-08-30 20:00:28 -0300

    Adiciona tab navigation

--- projetos/gofinances/src/routes/app.routes.tsx
+++ projetos/gofinances/src/routes/app.routes.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useTheme } from "styled-components";
+import { Platform } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons/";
 
 const Tab = createBottomTabNavigator();
 
@@ -7,11 +10,52 @@ import Dashboard from "../screens/Dashboard";
 import { Register } from "../screens/Register";
 
 export function AppRoutes() {
+  const theme = useTheme();
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Listagem" component={Dashboard} />
-      <Tab.Screen name="Register" component={Register} />
-      <Tab.Screen name="Lista" component={Register} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: "beside-icon",
+        tabBarStyle: {
+          height: 88,
+          paddingVertical: Platform.OS === "ios" ? 20 : 0,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Register"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Lista"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
