3ae33f32f5d8425a58c2090103b0eacfcf2bc71e
brunomorais <bruno.morais@gmail.com.br>
2022-11-08 18:26:53 -0300

    Usa o platform para fazer renderização condicional do botão entrar com apple

--- projetos/gofinances/src/screens/SignIn/index.tsx
+++ projetos/gofinances/src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { ActivityIndicator, Alert } from "react-native";
+import { ActivityIndicator, Alert, Platform } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { useTheme } from "styled-components";
 import AppleSvg from "../../assets/apple.svg";
@@ -65,11 +65,13 @@ export const SignIn: React.FC = () => {
             svg={GoogleSvg}
             onPress={handleSingInWithGoogle}
           />
-          <SignInSocialButton
-            title="Entrar com Apple"
-            svg={AppleSvg}
-            onPress={handleSingInWithApple}
-          />
+          {Platform.OS === "ios" && (
+            <SignInSocialButton
+              title="Entrar com Apple"
+              svg={AppleSvg}
+              onPress={handleSingInWithApple}
+            />
+          )}
         </FooterWrapper>
 
         {isLoading && (
