d8d37e0c1c345c19561d6e1637c1876f60b5d4e3
brunomorais <bruno.morais@gmail.com.br>
2022-11-14 11:22:01 -0300

    Configura as fontes e o tema global

--- projetos/rentx/src/screens/Home/index.tsx
+++ projetos/rentx/src/screens/Home/index.tsx
@@ -4,7 +4,7 @@ import { Container, Title } from "./styles";
 const Home: React.FC = () => {
   return (
     <Container>
-      <Title>Testando teste teste</Title>
+      <Title>Testando</Title>
     </Container>
   );
 };
