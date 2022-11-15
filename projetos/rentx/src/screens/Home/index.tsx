05f6a50fe5866a651cb16d97f95e1a23f9eec164
brunomorais <bruno.morais@gmail.com.br>
2022-11-15 10:11:07 -0300

    Adiciona os assets, os arquivos de tipagem svg e png

--- projetos/rentx/src/screens/Home/index.tsx
+++ projetos/rentx/src/screens/Home/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import { Container, Header, TotalCars, HeaderContent } from "./styles";
+import { StatusBar } from "react-native";
+
+import Logo from "../../assets/logo.svg";
+import { RFValue } from "react-native-responsive-fontsize";
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <Title>Testando</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 };
