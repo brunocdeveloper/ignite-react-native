bd11ceaabd61d6de3015f98700130b24b05a12d5
brunomorais <bruno.morais@gmail.com.br>
2022-11-17 22:10:26 -0300

    Cria os icones descritivos do carro

--- projetos/rentx/src/screens/CarDetails/index.tsx
+++ projetos/rentx/src/screens/CarDetails/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { View } from "react-native";
 import BackButton from "../../components/BackButton";
 import ImageSlider from "../../components/ImageSlider";
+import Accessory from "../../components/Accessory";
+
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
 
 import {
   CarImages,
@@ -16,6 +23,7 @@ import {
   Period,
   Price,
   About,
+  Accessories,
 } from "./styles";
 
 const CarDetails = () => {
@@ -45,6 +53,15 @@ const CarDetails = () => {
             <Price>R$ 580</Price>
           </Rent>
         </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
 
         <About>
           Este é automóvel desportivo. Surgiu do lendário touro de lide
