23059788b22af11c4ab99b429aae4cf4992d7ac4
brunomorais <bruno.morais@gmail.com.br>
2022-11-17 21:46:07 -0300

    Cria a interface de detalhes do carrossel

--- projetos/rentx/src/screens/CarDetails/index.tsx
+++ projetos/rentx/src/screens/CarDetails/index.tsx
@@ -3,7 +3,20 @@ import { View } from "react-native";
 import BackButton from "../../components/BackButton";
 import ImageSlider from "../../components/ImageSlider";
 
-import { CarImages, Container, Header } from "./styles";
+import {
+  CarImages,
+  Container,
+  Header,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from "./styles";
 
 const CarDetails = () => {
   return (
@@ -19,6 +32,26 @@ const CarDetails = () => {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   );
 };
