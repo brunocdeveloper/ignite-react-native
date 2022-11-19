4b24499a7f3c3c8430c6a63731455d4cd48e41d3
brunomorais <bruno.morais@gmail.com.br>
2022-11-19 15:30:31 -0300

    Cria a screen de carro alugado

--- projetos/rentx/App.js
+++ projetos/rentx/App.js
@@ -16,6 +16,7 @@ import Home from "./src/screens/Home";
 import theme from "./src/styles/theme";
 import Scheduling from "./src/screens/Scheduling";
 import SchedulingDetails from "./src/screens/SchedulingDetails";
+import SchedulingComplete from "./src/components/SchedulingComplete";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -32,7 +33,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   );
 }
