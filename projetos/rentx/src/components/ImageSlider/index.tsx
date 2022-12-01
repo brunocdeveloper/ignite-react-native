6a0f3a7ec793d9460ee1ab97877c6bcb40225b7c
brunomorais <bruno.morais@gmail.com.br>
2022-12-01 10:20:02 -0300

    Utiliza useRef no onviewableItemsChanged do flatlist para alterar o index do slide atual

--- projetos/rentx/src/components/ImageSlider/index.tsx
+++ projetos/rentx/src/components/ImageSlider/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { FlatList } from "react-native";
+import React, { useRef, useState } from "react";
+import { FlatList, ViewToken } from "react-native";
 
 import {
   Container,
@@ -13,12 +13,23 @@ interface Props {
   imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 const ImageSlider = ({ imagesUrl }: Props) => {
+  const [imageIndex, setImageIndex] = useState(0);
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    const index = info.viewableItems[0].index!;
+    setImageIndex(index);
+  });
+
   return (
     <Container>
       <ImageIndexes>
         {imagesUrl.map((_, index) => (
-          <ImageIndex key={index} active={true} />
+          <ImageIndex key={index} active={index === imageIndex} />
         ))}
       </ImageIndexes>
 
@@ -32,6 +43,7 @@ const ImageSlider = ({ imagesUrl }: Props) => {
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={indexChanged.current}
       />
     </Container>
   );
