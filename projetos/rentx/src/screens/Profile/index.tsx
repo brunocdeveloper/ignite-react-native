87ab5a0314f13d8bc4c7a72bfae980375fd72a63
brunomorais <bruno.morais@gmail.com.br>
2023-01-22 14:20:36 -0300

    Cria as seções de editar dados e trocar a senha do usuário

--- projetos/rentx/src/screens/Profile/index.tsx
+++ projetos/rentx/src/screens/Profile/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
 import { useTheme } from "styled-components/native";
 import BackButton from "../../components/BackButton";
 import { Feather } from "@expo/vector-icons";
@@ -12,9 +12,26 @@ import {
   PhotoContainer,
   Photo,
   PhotoButton,
+  Content,
+  Options,
+  Option,
+  OptionTitle,
+  Section,
 } from "./styles";
+import Input from "../../components/Input";
+import { KeyboardAvoidingView } from "react-native";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import PasswordInput from "../../components/PasswordInput";
+import { useAuth } from "../../hooks/auth";
+
+enum OptionType {
+  "DATA_EDIT" = "dataEdit",
+  "PASSWDORD_EDIT" = "passwordEdit",
+}
 
 const Profile = () => {
+  const [option, setOption] = useState<"dataEdit" | "passwordEdit">("dataEdit");
+  const { user } = useAuth();
   const theme = useTheme();
   const navigation = useNavigation();
 
@@ -24,29 +41,97 @@ const Profile = () => {
 
   const handleSignOut = () => {};
 
-  return (
-    <Container>
-      <Header>
-        <HeaderTop>
-          <BackButton color={theme.colors.shape} onPress={handleBack} />
-          <HeaderTitle>Editar Perfil</HeaderTitle>
-          <LogoutButton onPress={handleSignOut}>
-            <Feather name="power" size={24} color={theme.colors.shape} />
-          </LogoutButton>
-        </HeaderTop>
+  const handleOptionChange = (option: "dataEdit" | "passwordEdit") => () => {
+    setOption(option);
+  };
 
-        <PhotoContainer>
-          <Photo
-            source={{
-              uri: "https://avatars.githubusercontent.com/u/72170223?v=4",
-            }}
-          />
-          <PhotoButton onPress={() => {}}>
-            <Feather name="camera" size={24} color={theme.colors.shape} />
-          </PhotoButton>
-        </PhotoContainer>
-      </Header>
-    </Container>
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <Container>
+        <Header>
+          <HeaderTop>
+            <BackButton color={theme.colors.shape} onPress={handleBack} />
+            <HeaderTitle>Editar Perfil</HeaderTitle>
+            <LogoutButton onPress={handleSignOut}>
+              <Feather name="power" size={24} color={theme.colors.shape} />
+            </LogoutButton>
+          </HeaderTop>
+
+          <PhotoContainer>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/72170223?v=4",
+              }}
+            />
+            <PhotoButton onPress={() => {}}>
+              <Feather name="camera" size={24} color={theme.colors.shape} />
+            </PhotoButton>
+          </PhotoContainer>
+        </Header>
+
+        <Content style={{ marginBottom: useBottomTabBarHeight() }}>
+          <Options>
+            <Option
+              active={option === "dataEdit"}
+              onPress={handleOptionChange("dataEdit")}
+            >
+              <OptionTitle active={option === "dataEdit"}>Dados</OptionTitle>
+            </Option>
+            <Option
+              active={option === "passwordEdit"}
+              onPress={handleOptionChange("passwordEdit")}
+            >
+              <OptionTitle active={option === "passwordEdit"}>
+                Trocar senha
+              </OptionTitle>
+            </Option>
+          </Options>
+
+          {option === OptionType.DATA_EDIT && (
+            <Section>
+              <Input
+                iconName="user"
+                placeholder="Nome"
+                autoCorrect={false}
+                defaultValue={user.name}
+              />
+              <Input
+                iconName="mail"
+                editable={false}
+                autoCorrect={false}
+                defaultValue={user.email}
+              />
+              <Input
+                iconName="credit-card"
+                placeholder="CNH"
+                keyboardType="numeric"
+                defaultValue={user.driver_license}
+              />
+            </Section>
+          )}
+
+          {option === OptionType.PASSWDORD_EDIT && (
+            <Section>
+              <PasswordInput
+                iconName="lock"
+                placeholder="Senha atual"
+                autoCorrect={false}
+              />
+              <PasswordInput
+                iconName="lock"
+                placeholder="nova senha"
+                autoCorrect={false}
+              />
+              <PasswordInput
+                iconName="lock"
+                placeholder="Repetir senha"
+                autoCorrect={false}
+              />
+            </Section>
+          )}
+        </Content>
+      </Container>
+    </KeyboardAvoidingView>
   );
 };
 
