ffa1176b91c1efa739ee0fd99cff9449b1661777
brunomorais <bruno.morais@gmail.com.br>
2023-02-07 21:50:10 -0300

    Adiciona testIgnorePatterns para o config do jest

--- projetos/gofinances/jest.config.js
+++ projetos/gofinances/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: "react-native",
+  testPathIgnorePatterns: ["/node_modules/", "/android/", "/ois"],
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
 };
